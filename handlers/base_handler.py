c61929f0d0d8dbf53ef3c9ff2a98cf8f249bfca4
Luke Zapart <luke@zapart.org>
2016-12-24 00:51:22 +0100

    Revert "Add bounds checking to BaseHandler.read()"
    This reverts commit 045ead44ef69d6ebf2cb0dddf084762efcc62995.

--- handlers/base_handler.py
+++ handlers/base_handler.py
@@ -8,10 +8,4 @@ class BaseHandler:
         self.info = OrderedDict()
 
     def read(self, offset, size):
-        if offset < 0:
-            raise IndexError("File offset must be greater than 0")
-
-        if offset + size >= len(self.file):
-            raise IndexError("Cannot read beyond the end of the file")
-
         return self.file[offset:offset + size]
